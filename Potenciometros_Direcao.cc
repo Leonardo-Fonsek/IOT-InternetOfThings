422b17cc285233948957e5da64c9c3b03300d830
Leonardo <leo-nino70@outlook.com>
2024-10-09 19:54:51 -0300

    Potenciometros_Direcao 2.0

--- Potenciometros_Direcao.cc
+++ Potenciometros_Direcao.cc
@@ -12,14 +12,16 @@ void loop() {
   float pot1Power = pot1Value * (5.0 / 1023.0);
   float pot2Power = pot2Value * (5.0 / 1023.0);
 
-  if (pot1Power < 1.25){
+  if (pot1Power < 2.00){
   	Serial.println("Para direita");
-  } else if (pot1Power < 2.50){
+  } else {
   	Serial.println("Para esquerda");
-  } else if (pot1Power < 3.75){
-  	Serial.println("Para cima");
-  } else if (pot1Power < 4.00){
-  	Serial.print("Para baixo");
   }
+  if (pot2Power < 2.00){
+  	Serial.println("Para cima");
+  } else {
+  	Serial.println("Para baixo");
+  }
+  
   delay(500);
 }
